b2756c9e3c7afc1dbfceb1fce0f20b00ef03fa4c
Andrew Schafer <Andrew.Schafer@genmills.com>
2020-03-05 14:57:46 -0600

    installed eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,37 +1,30 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-
-    ]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
 const App = () => {
-    return React.createElement(
-        "div", {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Zoey",
-                animal: "Dog",
-                breed: "Rescue"
-            }),
-            React.createElement(Pet, {
-                name: "Sparty",
-                animal: "Dog",
-                breed: "Golden Retriever"
-            }),
-            React.createElement(Pet, {
-                name: "Walter",
-                animal: "Dog",
-                breed: "Chocolate Lab"
-            })
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Zoey",
+      animal: "Dog",
+      breed: "Rescue"
+    }),
+    React.createElement(Pet, {
+      name: "Sparty",
+      animal: "Dog",
+      breed: "Golden Retriever"
+    }),
+    React.createElement(Pet, {
+      name: "Walter",
+      animal: "Dog",
+      breed: "Chocolate Lab"
+    })
+  ]);
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
