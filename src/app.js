67abe3f8b441992082eb6856a35b695a879c4112
Andrew Schafer <Andrew.Schafer@genmills.com>
2020-03-05 15:22:10 -0600

    Break project up into modules

--- src/app.js
+++ src/app.js
@@ -1,30 +1,28 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from 'react';
+import {
+    render
+} from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Zoey",
-      animal: "Dog",
-      breed: "Rescue"
-    }),
-    React.createElement(Pet, {
-      name: "Sparty",
-      animal: "Dog",
-      breed: "Golden Retriever"
-    }),
-    React.createElement(Pet, {
-      name: "Walter",
-      animal: "Dog",
-      breed: "Chocolate Lab"
-    })
-  ]);
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Zoey",
+            animal: "Dog",
+            breed: "Rescue"
+        }),
+        React.createElement(Pet, {
+            name: "Sparty",
+            animal: "Dog",
+            breed: "Golden Retriever"
+        }),
+        React.createElement(Pet, {
+            name: "Walter",
+            animal: "Dog",
+            breed: "Chocolate Lab"
+        })
+    ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
