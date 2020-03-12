97edfef3e214e1fbb4b149296670233b5e87580c
schaferandrew <schaferandrew@outlook.com>
2020-03-11 21:12:54 -0500

    WIP Hooks

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
-
+import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <div>
-      <h1 id="something-important"> Adopt Me! </h1>
-      <Pet name="Zoey" animal="Dog" breed="Rescue"></Pet>
-      <Pet name="Sparty" animal="Dog" breed="Golden Retriever"></Pet>
-      <Pet name="Walter" animal="Dog" breed="Chocolate Lab"></Pet>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
     </div>
   );
 };
